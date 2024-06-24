a23635cf5119727c575c6ba5eb6493addda0e0b4
yaylily <yeewooooon@gmail.com>
2024-06-24 10:39:41 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -2,17 +2,20 @@ function solution(k, score) {
     const honor = []
     const result = []
 
-    for(let i = 0 ; i < score.length ; i ++) {
+    for(let i=0; i < score.length; i++){
         if(i < k) {
             honor.push(score[i])
         }
-
-        if(score[i]>Math.min(...honor)) {
+        
+        if(score[i] > Math.min(...honor)){
             honor.pop()
             honor.push(score[i])
-            honor.sort((a,b) => b-a)
+            honor.sort((a, b) => b-a)
         }
-        result.push(honor.at(-1))
+        
+        result.push(honor[honor.length -1])
     }
+    
     return result
+
 }
