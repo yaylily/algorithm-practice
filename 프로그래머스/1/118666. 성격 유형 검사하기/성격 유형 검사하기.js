e8b3e5f1fc432c51e1081996d3476ec251a41cdc
yaylily <yeewooooon@gmail.com>
2024-07-09 10:14:39 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.62 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -0,0 +1,30 @@
+function solution(survey, choices) {
+
+    const types = {R:0, T:0, C:0, F:0, J:0, M:0, A:0, N:0}
+    let answer = '';
+    
+    for(let i =0; i<choices.length; i++){
+        const [leftType, rightType] = survey[i].split('');
+        const result = Math.abs(choices[i]-4)
+        
+        if(choices[i] < 4) {
+            types[leftType] += result
+        } else {
+            types[rightType] += result
+        }
+    }
+    
+    const type = Object.keys(types);
+    for(let i=0; i<type.length; i += 2){
+        const leftType = types[type[i]];
+        const rightType = types[type[i+1]];
+        
+        if(leftType >= rightType){
+            answer += type[i]
+        } else {
+            answer += type[i+1]
+        }
+    }
+        
+    return answer;
+}
