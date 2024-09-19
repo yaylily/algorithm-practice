69e04f1f7be6b116c053dbb2402894022b29780b
yaylily <yeewooooon@gmail.com>
2024-09-19 14:13:21 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -1,11 +1,14 @@
 function solution(dot) {
-    if(dot[0] > 0 && dot[1] > 0) {
+    const x = dot[0]
+    const y = dot[1]
+    
+    if( x > 0 && y > 0) {
         return 1
-    } else if(dot[0] < 0 && dot[1] > 0) {
+    } else if(x < 0 && y > 0) {
         return 2
-    } else if(dot[0] < 0 && dot[1] < 0) {
+    } else if(x < 0 && y < 0) {
         return 3
-    } else if(dot[0] > 0 && dot[1] < 0) {
+    } else if(x > 0 && y < 0) {
         return 4
     }
 }
