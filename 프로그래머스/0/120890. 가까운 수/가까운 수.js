3202b7ab66a7bd4ffaed0090b11fc005e7734709
yaylily <yeewooooon@gmail.com>
2024-09-29 00:03:28 +0900

    [level 0] Title: 가까운 수, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.js
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.js
@@ -1,13 +1,4 @@
 function solution(array, n) {
-    return array.reduce((prev, curr) => {
-        const diffPrev = Math.abs(prev-n);
-        const diffCurr = Math.abs(curr-n)
-        if(diffCurr < diffPrev){
-            return curr
-        } else if(diffCurr===diffPrev){
-            return prev<curr ? prev : curr;
-        } else {
-            return prev
-        }
-    })
+    array.sort((a, b) => Math.abs(n-a) - Math.abs(n-b) || a-b)
+    return array[0]
 }
