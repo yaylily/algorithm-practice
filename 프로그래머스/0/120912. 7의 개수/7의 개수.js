70c0e75273dda76962247309f146a3bc5762d23f
yaylily <yeewooooon@gmail.com>
2024-10-06 18:10:58 +0900

    [level 0] Title: 7의 개수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.js
@@ -1,13 +1,3 @@
 function solution(array) {
-    let answer = 0
-    for(let a of array){
-        const strArr = String(a).split("")
-        for(let i=0; i<strArr.length; i++){
-            if(strArr[i] === "7"){
-                answer += 1
-            }
-        }
-    }
-    
-    return answer
+    return array.join("").split("").filter((a) => a === "7").length
 }
