b373067467281a5b64d6beb0e8b5cfd4993fd7e6
yaylily <yeewooooon@gmail.com>
2024-10-11 17:03:20 +0900

    [level 0] Title: 평행, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -0,0 +1,17 @@
+function solution(dots) {
+    const [a, b, c, d] = dots
+    
+    if(slope(a, b) === slope(c, d))
+        return 1
+    if(slope(a, c) === slope(b, d))
+        return 1
+    if(slope(a, d) === slope(b, c))
+        return 1
+    
+    return 0
+    
+}
+
+function slope(arr1, arr2){
+    return (arr2[1] - arr1[1]) / (arr2[0] - arr1[0])
+}
