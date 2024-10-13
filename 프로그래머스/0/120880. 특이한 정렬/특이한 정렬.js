748f72214502eb815385a9d826a1fff6fb5d3614
yaylily <yeewooooon@gmail.com>
2024-10-13 15:53:34 +0900

    [level 0] Title: 특이한 정렬, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,19 +1,3 @@
 function solution(numlist, n) {
-    let differences =[]
-    
-    for(let i=0; i<numlist.length; i++){
-        differences[i] = []
-        differences[i][0] = numlist[i]
-        differences[i][1] = Math.abs(numlist[i] - n)
-    }
-   
-    differences.sort((a, b) => {
-        if(a[1] === b[1]){
-            return b[0] - a[0]
-        } else {
-            return a[1] - b[1]
-        }
-    })
-    
-    return differences.map((a) => a[0])
+    return numlist.sort((a, b) => Math.abs(a-n) - Math.abs(b-n) || b-a)
 }
