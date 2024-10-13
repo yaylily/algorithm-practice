3986ff0116a664848b8f11305e150a7f36037ded
yaylily <yeewooooon@gmail.com>
2024-10-13 15:23:51 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,3 +1,15 @@
 function solution(a, b) {
-    return Number((a/b).toFixed(10)) == a/b ? 1 : 2
+    let n=1
+    for(let i=1; i<=Math.min(a, b); i++){
+        if(a%i === 0 && b%i === 0){
+            n=i
+        }
+    }
+    
+    b /= n
+    
+    while(b%2 === 0) b/=2
+    while(b%5 === 0) b/=5
+    
+    return b===1 ? 1 : 2
 }
