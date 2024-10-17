e20f35bdab0f1ffee74451f5f86f9b2461af5ed9
yaylily <yeewooooon@gmail.com>
2024-10-17 23:25:15 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.09 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,9 +1,7 @@
 function solution(common) {
     if(common[1]-common[0] === common[2]-common[1]){
-        const difference1 = common[1]-common[0]
-        return common[0] + (difference1*common.length)
+        return common.pop() + (common[1]-common[0])
     } else {
-        const difference2 = common[1]/common[0]
-        return common[0] * (difference2 ** common.length)
+        return common.pop() * (common[1]/common[0])
     }
 }
