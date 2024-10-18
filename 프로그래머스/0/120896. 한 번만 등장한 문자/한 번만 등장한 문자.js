ef2c9d22a369388c3f6a2acc0cdc1167107f3a95
yaylily <yeewooooon@gmail.com>
2024-10-18 23:32:26 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,23 +1,11 @@
 function solution(s) {
-    let answer = ''
-    let temp = {}
+    let several = []
+    const asc = [...s].sort()
     
-    //한 번만 등장하는 문자 찾기
-    for(let char of s){
-        if(char in temp){
-            temp[char] +=1
-        } else {
-            temp[char] = 1
+    for(let i=0; i<asc.length; i++){
+        if(asc[i] === asc[i+1]){
+            several.push(asc[i])
         }
     }
-    //객체 값이 1인 것 찾기
-    for(let t in temp){
-        if(temp[t] === 1){
-            answer += t
-        }
-    }
-    
-    //문자 오름차순
-    return answer.split("").sort().join("")
-    
+    return asc.filter((a) => !several.includes(a)).join("")
 }
