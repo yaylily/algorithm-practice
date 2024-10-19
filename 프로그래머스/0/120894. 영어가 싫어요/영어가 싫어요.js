99963af02eab0990bc3b021a2e2db0d392a2cd58
yaylily <yeewooooon@gmail.com>
2024-10-20 01:03:35 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,12 +1,11 @@
 function solution(numbers) {
-    const engNum = { "zero":0, "one":1, "two":2, "three":3, "four":4,
-                    "five":5, "six":6, "seven":7, "eight":8, "nine":9}
-    let answer = numbers
+    const engNum = {"zero":0, "one":1, "two":2, "three":3, "four":4,
+                   "five":5, "six":6, "seven":7, "eight":8, "nine":9}
     
-    for(let word in engNum){
-        while(answer.includes(word)){
-            answer = answer.replace(word, engNum[word])
+    for(let n in engNum){
+        while(numbers.includes(n)){
+            numbers = numbers.replace(n, engNum[n])
         }
     }
-    return Number(answer)
+    return +numbers
 }
