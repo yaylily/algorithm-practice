bb41d9174d46cc5d21008781b60a6616cb6eaae3
yaylily <yeewooooon@gmail.com>
2024-10-23 21:07:56 +0900

    [level 0] Title: 다항식 더하기, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
+++ 프로그래머스/0/120863. 다항식 더하기/다항식 더하기.js
@@ -1,20 +1,25 @@
 function solution(polynomial) {
+    let answer = ""
+    const poly = polynomial.split(" + ");
+    let xSum = 0;
+    let numSum = 0;
     
-    const lineArr = polynomial.split(" ")
-    let xSum = 0
-    let numSum = 0
-    
-    for(let i=0; i<lineArr.length; i+=2){
-        if(lineArr[i].includes("x")){
-            let xNum = lineArr[i].split("x")[0]
-            xSum += xNum === "" ? 1 : Number(xNum)
+    for(let p of poly){
+        if(p.includes("x")){
+            const xValue = p.split("x")[0]
+            xSum += xValue === "" ? 1 : +xValue
         } else {
-            numSum += Number(lineArr[i])
+            numSum += +p
         }
     }
-    let result = []
-    if(xSum > 0) result.push(xSum===1 ? "x" : xSum + "x")
-    if(numSum > 0) result.push(numSum)
     
-    return result.join(" + ")
+    if(xSum > 0){
+        answer += xSum === 1 ? "x" : xSum + "x"
+    }
+    if(numSum > 0){
+        answer += xSum>0 ? " + " + numSum : numSum
+    }
+    
+    return answer
+    
 }
