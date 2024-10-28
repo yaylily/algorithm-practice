06902cf8e5ea0e9a4a2ebae79bd563d603edf66f
yaylily <yeewooooon@gmail.com>
2024-10-28 12:33:58 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,10 @@
 function solution(n) {
-    var answer = 0;
-    for(let i=0; i<=n; i++){
-        if (n % i === 0){
-            answer += i
+    let sum = 0;
+    
+    for(let i=1; i<=n; i++){
+        if(n%i === 0){
+            sum += i
         }
     }
-    return answer;
+    return sum
 }
