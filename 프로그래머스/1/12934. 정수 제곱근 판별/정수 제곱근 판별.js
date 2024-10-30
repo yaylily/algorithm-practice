aa43e738cff25477fb3063696fe2cd0e8af8c955
yaylily <yeewooooon@gmail.com>
2024-10-30 13:31:50 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,13 +1,3 @@
 function solution(n) {
-    var answer = 0;
-    
-    for(let x=1; (x*x) <= n; x++){
-        if(n === x*x){
-            answer = (x+1)*(x+1)
-        } else{
-            answer = -1
-        };
-    };
-
-    return answer;
+    return Math.sqrt(n) % 1 === 0 ? (Math.sqrt(n) + 1) ** 2 : -1
 }
