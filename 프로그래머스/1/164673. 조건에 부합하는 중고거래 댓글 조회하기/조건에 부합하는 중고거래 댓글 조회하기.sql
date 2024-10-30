9b1a27156a8fce8e1081a5b65ae801a61324d16a
yaylily <yeewooooon@gmail.com>
2024-10-30 17:47:50 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,6 +1,5 @@
--- 코드를 입력하세요
-SELECT ub.TITLE, ub.BOARD_ID, ur.REPLY_ID, ur.WRITER_ID, ur.CONTENTS, date_format(ur.CREATED_DATE, '%Y-%m-%d') as CREATED_DATE
+select ub.TITLE, ub.BOARD_ID, ur.REPLY_ID, ur.WRITER_ID, ur.CONTENTS, date_format(ur.CREATED_DATE, '%Y-%m-%d')
 from USED_GOODS_BOARD ub
 inner join USED_GOODS_REPLY ur on ur.BOARD_ID = ub.BOARD_ID
-where year(ub.CREATED_DATE) = 2022 and month(ub.CREATED_DATE) = 10
+where ub.CREATED_DATE like '2022-10-%'
 order by ur.CREATED_DATE asc, ub.TITLE asc
