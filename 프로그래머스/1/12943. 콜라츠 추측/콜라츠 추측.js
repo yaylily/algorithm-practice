149fe185c618b3ab9b01426f453eb65155490618
yaylily <yeewooooon@gmail.com>
2024-10-31 12:12:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -2,10 +2,10 @@ function solution(num) {
     let count = 0;
 
     while(count < 500){
-        if(num ===1) break;
+        if(num ===1) return count;
         num = num%2 === 0 ? num/2 : num*3 +1
         
         count++
     }
-    return count === 500 ? -1 : count
+    return -1
 }
