35272811a1f0cc5a2d8dc97ca93e09fd70c23cfe
yaylily <yeewooooon@gmail.com>
2024-11-01 10:22:53 +0900

    [level 1] Title: 내적, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,8 +1,9 @@
 function solution(a, b) {
-    var answer = 0;
-    for(let i=0; i < a.length; i++){
-        answer += a[i]*b[i];
+    let sum = 0;
+    
+    for(let i=0; i<a.length; i++){
+        sum += a[i]*b[i]
     }
     
-    return answer;
+    return sum
 }
