60191997581fdebf0f8e52973b86dd5348645168
yaylily <yeewooooon@gmail.com>
2024-11-05 14:03:27 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 4.27 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,18 +1,9 @@
 function solution(t, p) {
-    let tPartArr = []
-    const pLen = p.length
+    let count = 0;
     
-    //p 길이의 t의 부분 문자열 배열 만들기
-    for(let i=0; i<t.length-(pLen-1) ; i++){
-        let temp = ''
-        for(let j=i; j<i+pLen; j++){
-             temp += t[j]
-        }
-        tPartArr.push(temp)    
+    for(let i=0; i<t.length-(p.length-1); i++){
+        const partT = t.slice(i, i+p.length)
+        if(+partT<= +p) count ++
     }
-
-    //p와 비교해서 작은 수 구하기
-    const smaller = tPartArr.filter((a) => a<=+p)
-    
-    return smaller.length
+    return count
 }
