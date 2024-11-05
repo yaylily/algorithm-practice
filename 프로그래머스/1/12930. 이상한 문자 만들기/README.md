138df5522792542077fcb3210225e3f0779a31e1
yaylily <yeewooooon@gmail.com>
2024-11-05 14:30:55 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/README.md
+++ 프로그래머스/1/12930. 이상한 문자 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 이상한 문자 만들기 - 12930 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12930#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.17 ms
+메모리: 33.4 MB, 시간: 0.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 29일 09:55:23
+2024년 11월 05일 14:30:52
 
 ### 문제 설명
 
--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,17 +1,14 @@
 function solution(s) {
-    let arr = s.split(" ");
-    let answer = "";
-    for(let i=0; i<arr.length; i++){
-        for(let j=0; j<arr[i].length; j++){
-            if(j % 2 !== 0){
-                answer = answer + arr[i][j].toLowerCase();
-            }else{
-                answer = answer + arr[i][j].toUpperCase();
-            }
+    
+    //각 배열 짝수 홀수 별로 대소문자 바꿔주기
+    const changedS = s.split(" ").map((a) => {
+        let char = a.split("")
+        for(let i=0; i<char.length; i++){
+            i%2 ===0 ? char[i] = char[i].toUpperCase() : char[i] = char[i].toLowerCase()
         }
-        if(i < arr.length - 1){
-                answer = answer + " ";
-            }
-    }
-    return answer;
+        return char.join("")
+    })
+    
+    //사이에 공백 넣어서 문자열로 합쳐주기
+    return changedS.join(" ")
 }
