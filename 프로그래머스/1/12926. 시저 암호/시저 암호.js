9542433cf1c8295bff8a289a6853b91be29cc59b
yaylily <yeewooooon@gmail.com>
2024-11-08 12:56:11 +0900

    [level 1] Title: 시저 암호, Time: 1.69 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,24 +1,22 @@
 function solution(s, n) {
-    var answer = '';
+    const smallAlph = ["a", "b", "c", "d" ,"e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const bigAlph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
     
-    for(let i=0; i<s.length;i++){
-        if(s[i]=== " "){
-            answer += " "
-        } else{
-            let asc = s.charCodeAt(i)
-            if(asc <= 90){
-                asc += n
-                if(asc>90){
-                    asc -= 26
-                }
-            } else{
-                asc += n;
-                if(asc > 122){
-                    asc -= 26
-                }
-            }   
-      answer += String.fromCharCode(asc);
-        }
-    }
-    return answer;
+   // 하나씩 돌려서 해당 값의 인덱스 구해줌
+    const siser = s.split("").map((a) => {
+        // 공백인 경우
+        if(a===" "){
+        return " "
+            
+        // 대문자인 경우
+        } else if(a === a.toUpperCase()){
+            let index = bigAlph.indexOf(a)
+            return  index+n <= 25 ? bigAlph[index+n] : bigAlph[index+n-26]
+        } else {
+            // 소문자인 경우
+            let index = smallAlph.indexOf(a)
+            return index+n <= 25 ? smallAlph[index+n] : smallAlph[index+n-26]
+        }})
+    
+    return siser.join("")
 }
