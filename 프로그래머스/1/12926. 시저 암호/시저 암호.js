066a65de8a68f05ea49eac2285866802133343ab
yaylily <yeewooooon@gmail.com>
2024-11-08 13:15:02 +0900

    [level 1] Title: 시저 암호, Time: 1.51 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -5,18 +5,15 @@ function solution(s, n) {
    // 하나씩 돌려서 해당 값의 인덱스 구해줌
     const siser = s.split("").map((a) => {
         // 공백인 경우
-        if(a===" "){
-        return " "
+        if(a===" ") return " "
+        
+        const type = smallAlph.includes(a) ? smallAlph : bigAlph
+        let index = type.indexOf(a) + n
+        
+        if(index >= type.length) index -= type.length
+        
+        return type[index]
+    })
             
-        // 대문자인 경우
-        } else if(a === a.toUpperCase()){
-            let index = bigAlph.indexOf(a)
-            return  index+n <= 25 ? bigAlph[index+n] : bigAlph[index+n-26]
-        } else {
-            // 소문자인 경우
-            let index = smallAlph.indexOf(a)
-            return index+n <= 25 ? smallAlph[index+n] : smallAlph[index+n-26]
-        }})
-    
     return siser.join("")
 }
