9136befb2986c07918509ad28c996a8a7b9f6f6f
yaylily <yeewooooon@gmail.com>
2024-11-11 13:52:16 +0900

    [level 1] Title: 콜라 문제, Time: 0.37 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -5,12 +5,9 @@ function solution(a, b, n) {
     // 남은 병수가 교환 가능한 수보다 작을 때까지 반복
     while(remain>=a){
         // 나누었을 때 정수 값을 마트에 줌
-        const give = remain % a !==0 ? remain-(remain % a) : remain
-        const get = give/a * b
-        count += get
-        
-        // 빈 병 나머지 값 remain 업데이트 시켜주기
-        remain = get + (remain - give)
+        count += Math.floor(remain/a)*b
+        // 빈 병 업데이트 시켜주기
+        remain = Math.floor(remain/a)*b + remain%a
     }
     
     return count
