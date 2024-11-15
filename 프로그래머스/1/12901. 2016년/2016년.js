934d9eaff96b8c7b4b562ae437f947c4b009b502
yaylily <yeewooooon@gmail.com>
2024-11-15 14:25:32 +0900

    [level 1] Title: 2016년, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,10 +1,8 @@
 function solution(a, b) {
-    let weeks = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
-    let months = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    let whatWeek = b - 1;
-    for (let i = 1; i < a; i++) {
-        whatWeek += months[i - 1];
-    }
-
-    return weeks[whatWeek % 7];
+    const week = [ "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" ]
+    const month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    const days = month.slice(0,a-1).reduce((acc, value) => acc+value, 0) + b
+    
+    return week[(days-1)%7]
 }
