9f75c656b1e0ce57117400f260ee0d7486681192
yaylily <yeewooooon@gmail.com>
2024-11-16 21:01:24 +0900

    [level 1] Title: 기사단원의 무기, Time: 105.76 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -1,21 +1,17 @@
 function solution(number, limit, power) {
-    var answer = 0;
+    let sum = 0
     
+    //약수의 개수를 구한다
     for(let i=1; i<=number; i++){
-        let count = 0
-        let sqrtNumber = Math.sqrt(i)
-        for(let j=1; j<=sqrtNumber;j++){
-            if(i%j === 0){
-            count++
-            if(j !== i /j){count++}
-            }}
-        
-        if(count > limit){
-            answer+= power
-        } else {
-            answer+=count
+        let temp = 0
+        for(let j=1; j<=Math.sqrt(i); j++){
+           if(i%j === 0) {
+               temp ++
+               if(i !== j*j) temp ++
+           }
         }
+    // 약수의 개수가 limit을 넘을 경우, power로 바꿔준다.  
+        temp > limit ? sum += power : sum += temp
     }
-    
-    return answer;
+    return sum
 }
