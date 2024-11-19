e9f6e47a4b2ba56eacabf8f51cc68a4b4d05053f
yaylily <yeewooooon@gmail.com>
2024-11-19 16:57:47 +0900

    [level 1] Title: 과일 장수, Time: 207.32 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,13 +1,15 @@
 function solution(k, m, score) {
+    if(score.length < m) return 0
+    
     //score 내림차순
     score = score.sort((a, b) => b-a)
     
     // m씩 더해가며 해당 박스의 최저 사과 점수 구함
-    let price = []
+    let total = 0
     for (let i=m-1; i<score.length; i+=m){
-        price.push(score[i]*m)
+        total += score[i]*m
     }
     
     //박스 점수 모두 더해서 반환
-    return price.reduce((a,b)=> a+b, 0)
+    return total
 }
