eb5aa073e13dec51b4c8687bb5deaaa9004efbfc
yaylily <yeewooooon@gmail.com>
2024-11-21 12:25:19 +0900

    [level 1] Title: 덧칠하기, Time: 3603.54 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,14 +1,11 @@
 function solution(n, m, section) {
-  let answer = 0;
-
-  let part = 0;
-
-  section.forEach((n) => {
-    if (n > part) {
-      part = n + m - 1;
-      answer++;
+    let count =0;
+    
+    for(let i=section[0]; i<=section[section.length-1]; i++){
+        if(section.includes(i)) {
+            i+=(m-1)
+             count ++            
+        }
     }
-  });
-
-  return answer;
+    return count
 }
