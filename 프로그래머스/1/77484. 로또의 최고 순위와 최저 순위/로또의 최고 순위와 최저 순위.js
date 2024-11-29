5f762f9002b090a14787bf3efc53e6b1a88af74d
yaylily <yeewooooon@gmail.com>
2024-11-29 17:09:43 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.js
@@ -1,33 +1,8 @@
 function solution(lottos, win_nums) {
-    var answer = [];
+    const rank = [6, 6, 5, 4, 3, 2, 1]
     
-    let maxRank = 0;
-    let minRank = 0;
+    const min = lottos.filter((a) => win_nums.includes(a)).length
+    const max = min + lottos.filter((a) => a === 0).length
     
-    minRank = lottos.filter((num)=> num !== 0 && win_nums.includes(num)).length;
-    maxRank = lottos.filter((num)=> num === 0 ).length + minRank;
-    
-    answer.push(findRank(maxRank));
-    answer.push(findRank(minRank));
-    
-        
-    return answer;
-}
-    
-    function findRank(count){
-        switch(count){
-            case 6:
-                return 1;
-            case 5:
-                return 2;
-            case 4:
-                return 3;
-            case 3:
-                return 4;
-            case 2:
-                return 5;
-            default:
-                return 6
-        }
-
+    return [rank[max], rank[min]]
 }
