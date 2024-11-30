3315dc277c51b19ff886a60af75317adff58771e
yaylily <yeewooooon@gmail.com>
2024-11-30 21:22:14 +0900

    [level 1] Title: 문자열 나누기, Time: 28.69 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -1,21 +1,19 @@
 function solution(s) {
-    let answer = []
+    let answer = 0
     
     let firstLetter = s[0];
-    let firstIndex = 0;
     let firstCount = 0; 
     let remainingCount = 0;    
      for(let i=0; i<s.length; i++){
         s[i] === firstLetter ? firstCount++ : remainingCount++;
          
         if(firstCount === remainingCount){
-            answer.push(s.slice(firstIndex, i+1))
+            answer++
             
-            firstIndex = i+1
             firstCount = 0;
             remainingCount = 0;
-            firstLetter = s[firstIndex]
+            firstLetter = s[i+1]
         }
     }
-    return firstCount === remainingCount ? answer.length : answer.length +1
+    return firstCount === remainingCount ? answer : answer +1
 }
