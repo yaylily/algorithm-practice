fe6228a715eb73cba567812fa1a65e69b7cab867
yaylily <yeewooooon@gmail.com>
2024-12-01 18:26:39 +0900

    [level 1] Title: 둘만의 암호, Time: 0.34 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,20 +1,18 @@
 function solution(s, skip, index) {
-    var answer = '';
-    
-    const Alph = 'abcdefghijklmnopqrstuvwxyz'.split('')
-    const arrS = s.split('')
-    const arrSkip = skip.split('')
-    const newAlph = Alph.filter(a => !arrSkip.includes(a))
-
+    let answer = ''
+    const alph = "abcdefghijklmnopqrstuvwxyz"
     
     for(let i=0; i<s.length; i++){
-        let currentChar = s[i]
-        let currentIndex = newAlph.indexOf(currentChar)
-        
-        let newPosition = (currentIndex + index) % newAlph.length;
-        
-        answer += newAlph[newPosition]
+        let current = alph.indexOf(s[i])
+        let steps = 0
+                  
+        while(steps < index){
+            current = (current+1) % 26
+            if(!skip.includes(alph[current])){
+                steps++
+            }
+        }
+        answer += alph[current]
     }
-    
-    return answer;
+    return answer
 }
