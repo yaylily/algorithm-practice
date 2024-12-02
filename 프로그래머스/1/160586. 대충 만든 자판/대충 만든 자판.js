dacc22f621b5f7d40a4c42fe2507e7957d1a877f
yaylily <yeewooooon@gmail.com>
2024-12-02 17:07:21 +0900

    [level 1] Title: 대충 만든 자판, Time: 23.80 ms, Memory: 38.5 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -1,26 +1,23 @@
 function solution(keymap, targets) {
-  let answer = [];
+  return targets.map((target) => {
+    let pressCount = 0      
+    
+      for(let i=0; i<target.length;i++){
+          let temp = Infinity
 
-  targets.forEach((target) => {
-     let press = 0;
-      
-      for (let i =0; i < target.length; i++){
-          let cnt = Infinity;
+          for(let key of keymap){
+            
+              if(key.includes(target[i])){
+                  temp = Math.min(temp, key.indexOf(target[i])+1)
+              }}
           
-          keymap.forEach((key) => {
-              const idx = key.indexOf(target[i]);
-              if (idx > -1) cnt = Math.min(cnt, idx+1);
-          })
-          
-          if( cnt === Infinity){
-              press = Infinity;
-              break;
-          } else {
-              press += cnt;
-          }
-      }
-      
-      answer.push(press === Infinity ? -1 : press);
-  });
-  return answer;
+                  
+            if(temp === Infinity){
+                return -1
+            }
+                  
+          pressCount += temp
+  }
+      return   pressCount
+    })
 }
