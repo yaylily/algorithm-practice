5cc7bb9896d0a2dcf14fafc2721ee2e71bdc2e67
yaylily <yeewooooon@gmail.com>
2024-12-04 12:52:25 +0900

    [level 1] Title: 체육복, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.js
+++ 프로그래머스/1/42862. 체육복/체육복.js
@@ -1,22 +1,19 @@
 function solution(n, lost, reserve) {
-
-    let realLost = lost.filter(l => !reserve.includes(l)).sort((a, b) => a - b);
-    let realReserve = reserve.filter(r => !lost.includes(r)).sort((a, b) => a - b);
     
-    let answer = n - realLost.length;
-
-    for (let i = 0; i < realLost.length; i++) {
-        let lostNum = realLost[i];
-      
-        for (let j = 0; j < realReserve.length; j++) {
-            let reserveNum = realReserve[j];
-            if (reserveNum === lostNum - 1 || reserveNum === lostNum + 1) {
-              answer += 1;
-              realReserve[j] = -1; 
-              break; 
-            }
+    let realLost = lost.filter((l) => !reserve.includes(l)).sort((a, b) => a-b)
+    let realReserve = reserve.filter((r) => !lost.includes(r)).sort((a, b) => a-b)
+    
+    let available = n-realLost.length
+    
+    realLost.forEach((l) => {
+        if(realReserve.includes(l-1)){
+            realReserve = realReserve.filter((r) => r !== l-1)
+            available ++
+        } else if(realReserve.includes(l+1)){
+            realReserve = realReserve.filter((r) => r !== l+1)
+            available ++
         }
-      }
-
-    return answer;
+    })
+    
+    return available  
 }
