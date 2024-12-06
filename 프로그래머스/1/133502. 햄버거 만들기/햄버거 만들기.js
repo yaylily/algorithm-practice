456c6faca811008f424279293741ee2d71263e12
yaylily <yeewooooon@gmail.com>
2024-12-06 13:54:46 +0900

    [level 1] Title: 햄버거 만들기, Time: 230.79 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,14 +1,16 @@
 function solution(ingredient) {
-    var answer = 0;
+    let count = 0;
+    
+    const pattern = '1231'
     let stack = []
     
-    for(let i of ingredient){
-        stack.push(i)
-        
-        if(stack.slice(-4).join('') === '1231'){
-            stack.splice(-4)
-            answer++
+    for(let i=0; i<ingredient.length; i++){
+        stack.push(ingredient[i])
+
+        if(stack.length >= 4 && stack.slice(-4).join("") === pattern){
+            count ++
+            stack.splice(-4, 4)
         }
     }
-    return answer;
+    return count
 }
