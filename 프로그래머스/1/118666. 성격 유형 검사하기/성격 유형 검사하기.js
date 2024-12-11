0d9bbf797913ad8ad379b69607990aa902201062
yaylily <yeewooooon@gmail.com>
2024-12-11 18:13:18 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 6.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,30 +1,30 @@
 function solution(survey, choices) {
-
-    const types = {R:0, T:0, C:0, F:0, J:0, M:0, A:0, N:0}
-    let answer = '';
+    let answer = ''
+    let personality = {R:0, T:0, C:0, F:0, J:0, M:0, A:0, N:0}
     
-    for(let i =0; i<choices.length; i++){
-        const [leftType, rightType] = survey[i].split('');
-        const result = Math.abs(choices[i]-4)
-        
-        if(choices[i] < 4) {
-            types[leftType] += result
-        } else {
-            types[rightType] += result
+    for(let i=0; i<choices.length; i++){
+        if(choices[i]<4){
+            personality[survey[i][0]] += Math.abs(choices[i]-4)
+        } else if(choices[i]>4){
+            personality[survey[i][1]] += (choices[i]-4)
         }
     }
+    console.log(personality)
+    const keys = Object.keys(personality)    
+    const values = Object.values(personality)
+    console.log(values)
+    console.log(keys)
     
-    const type = Object.keys(types);
-    for(let i=0; i<type.length; i += 2){
-        const leftType = types[type[i]];
-        const rightType = types[type[i+1]];
-        
-        if(leftType >= rightType){
-            answer += type[i]
+    for(let i=0; i<keys.length; i+=2){
+        if(values[i] > values[i+1]){
+            answer += keys[i]
+        } else if(values[i] < values[i+1]){
+            answer += keys[i+1]
         } else {
-            answer += type[i+1]
+            const same = [keys[i], keys[i+1]].sort()
+            answer += same[0]
         }
     }
-        
-    return answer;
+    return answer
+    
 }
