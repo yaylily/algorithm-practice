b5fe0feac7550a2b589d40f03abb45e0b7d2d614
yaylily <yeewooooon@gmail.com>
2024-12-15 14:33:37 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.33 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,22 +1,26 @@
 function solution(wallpaper) {
-    var answer = [];
-    
-    const X = []
-    const Y = []
+    let lux = 0
+    let luy = 0
+    let rdx = 0
+    let rdy = 0
     
     for(let i=0; i<wallpaper.length; i++){
         for(let j=0; j<wallpaper[i].length; j++){
             if(wallpaper[i][j] === "#"){
-                X.push(i);
-                Y.push(j)
+                // 처음 기준
+                if(lux===0 && luy===0 && rdx===0 && rdy===0){
+                lux = i
+                luy = j
+                rdx = i+1
+                rdy = j+1
+                }
+                // 기준점 보다 작거나 큰 경우 갱신
+                if(i < lux) lux = i
+                if(j < luy) luy = j
+                if(i+1 > rdx) rdx = i+1
+                if(j+1 > rdy) rdy = j+1
             }
         }
-    }   
-    
-    X.sort((a, b) => a - b)
-    Y.sort((a, b) => a - b)
-    
-    answer = [X[0], Y[0], X[X.length-1]+1, Y[Y.length-1]+1]
-    
-    return answer;
+    }
+    return [lux, luy, rdx, rdy]
 }
