a73c81ea67c6c70308540f40f64c15e6df562fec
yaylily <yeewooooon@gmail.com>
2024-12-16 23:44:19 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,40 +1,39 @@
 function solution(today, terms, privacies) {
-    var answer = [];
+   let answer = []
+ 
+   const [year, month, day] = today.split(".").map(Number)
+   let termsObj = {}
+   
+   for(let term of terms){
+       const [version, period] = term.split(" ")
+       termsObj[version] = +period
+   }
     
-    const [todayYear, todayMonth, todayDay]= today.split(".").map(Number)
-    const todayTotalDays = todayYear*12*28 + todayMonth*28 + todayDay
-    
-    const termMap = {}
-    terms.forEach(term =>{
-        const [type, duration] = term.split(" ")
-        termMap[type] = parseInt(duration)
-    })
-    
-    privacies.forEach((privacy, index) => {
-        const [date, type] = privacy.split(" ")
-        const [year, month, day] = date.split(".").map(Number)
+    for(let i=0; i<privacies.length; i++){
+        let [privacy, expiration] = privacies[i].split(" ")
+        let [privacyYear, privacyMonth, privacyDay] = privacy.split(".").map(Number)
         
-        const duration = termMap[type]
-        let expireYear = year
-        let expireMonth = month + duration
-        let expireDay = day-1
-        
-        if( expireDay === 0 ){
-            expireDay = 28;
-            expireMonth -= 1
+        privacyMonth += termsObj[expiration]
+        while(privacyMonth > 12){
+            privacyYear += 1
+            privacyMonth -= 12
         }
         
-        if(expireMonth > 12){
-            expireMonth -= 12
-            expireYear += 1
+        privacyDay -= 1
+        if(privacyDay === 0){
+            privacyMonth -= 1
+            privacyDay += 28
+            if(privacyMonth === 0){
+                privacyMonth += 12
+                privacyYear -= 1
+            }
         }
         
-        const expireTotalDays = expireYear*12*28 + expireMonth*28+expireDay
-        
-        if(expireTotalDays < todayTotalDays){
-            answer.push(index + 1)
-        }
-    })
-    
-    return answer;
+        if(privacyYear < year ||
+           (privacyYear === year && privacyMonth < month) ||
+          (privacyYear === year && privacyMonth === month && privacyDay < day)) {
+                 answer.push(i+1)
+            }
+    }   
+   return answer
 }
