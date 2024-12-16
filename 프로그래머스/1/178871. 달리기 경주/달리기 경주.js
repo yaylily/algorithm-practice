559480cdcb875c71a9e19874c00656a9771d6c72
yaylily <yeewooooon@gmail.com>
2024-12-17 07:59:53 +0900

    [level 1] Title: 달리기 경주, Time: 211.28 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -1,23 +1,19 @@
 function solution(players, callings) {
-    var answer = [];
-    
-    //인덱스를 담은 players 객체를 만든다.
-    let playerIndexMap = {}
+    const playersPosition = new Map()
     players.forEach((player, index) => {
-        playerIndexMap[player] = index
+        playersPosition.set(player, index)
     })
     
-    //callings를 하나씩 돌린다.
-    callings.forEach((callings) => {
-        let index = playerIndexMap[callings]
-        if(index>0){
-            [players[index-1], players[index]] = [players[index], players[index-1]]
-       
-        playerIndexMap[players[index]] = index
-        playerIndexMap[players[index-1]] = index -1
-         }
+    callings.forEach((call) => {
+        const currentIndex = playersPosition.get(call)
+        const previousPlayer = players[currentIndex-1]
+        
+        players[currentIndex] = previousPlayer
+        players[currentIndex-1] = call
+        
+        playersPosition.set(call, currentIndex-1)
+        playersPosition.set(previousPlayer, currentIndex)
     })
-    answer = players
     
-    return answer;
+    return players
 }
